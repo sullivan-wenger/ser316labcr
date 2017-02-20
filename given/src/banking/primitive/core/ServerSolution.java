7fae0f5893a3b8ed9e2239820d8bf53928b565a4
sullivan-wenger <swenger@asu.edu>
2017-02-19 20:41:55 -0700

    Update ServerSolution.java

--- given/src/banking/primitive/core/ServerSolution.java
+++ given/src/banking/primitive/core/ServerSolution.java
@@ -71,7 +71,9 @@ class ServerSolution implements AccountServer {
 	public boolean newAccount(String type, String name, float balance) 
 		throws IllegalArgumentException {
 		
-		if (balance < 0.0f) throw new IllegalArgumentException("New account may not be started with a negative balance");
+		if (balance < 0.0f) {
+			throw new IllegalArgumentException("New account may not be started with a negative balance");
+		}
 		
 		return newAccountFactory(type, name, balance);
 	}
