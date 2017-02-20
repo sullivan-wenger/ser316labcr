e5ab710c72a7e7ef747bf1cf8a82c0a4675fff5f
mjcorles <mjcorles>
2017-02-20 11:55:27 -0700

    Put private variables in Checking.java at end of class to reflect code guidelines

--- given/src/banking/primitive/core/Checking.java
+++ given/src/banking/primitive/core/Checking.java
@@ -1,9 +1,6 @@
 package banking.primitive.core;
 
 public class Checking extends Account {
-
-	private static final long serialVersionUID = 11L;
-	private int numWithdraws = 0;
 	
 	private Checking(String name) {
 		super(name);
@@ -58,4 +55,7 @@ public class Checking extends Account {
 	public String toString() {
 		return "Checking: " + getName() + ": " + getBalance();
 	}
+	
+	private static final long serialVersionUID = 11L;
+	private int numWithdraws = 0;
 }
