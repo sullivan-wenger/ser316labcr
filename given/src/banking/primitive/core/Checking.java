6116e11b5ea0ff23ce95348688fb08207b8232e2
mjcorles <mjcorles>
2017-02-20 11:39:31 -0700

    Put class variables at bottom of class

--- given/src/banking/primitive/core/Checking.java
+++ given/src/banking/primitive/core/Checking.java
@@ -1,18 +1,36 @@
 package banking.primitive.core;
 
 public class Checking extends Account {
-
-	private static final long serialVersionUID = 11L;
-	private int numWithdraws = 0;
 	
+	/**
+	  Method: Constructor for Checking class
+	  Inputs: name of Checking account
+	  Returns: N/A
+
+	  Description: Utilizes Constructor from parent class Account.java.
+	*/
 	private Checking(String name) {
 		super(name);
 	}
+	
+	/**
+	  Method: createChecking
+	  Inputs: name of account
+	  Returns: Checking object
 
+	  Description: Creates a new checking account.
+	*/
     public static Checking createChecking(String name) {
         return new Checking(name);
     }
-
+    
+    /**
+	    Method: Checking
+	    Inputs: name: name of account, balance: starting balance of account
+	    Returns: N/A
+	
+	    Description: Creates a Checking account with the given name and balance.
+    */
 	public Checking(String name, float balance) {
 		super(name, balance);
 	}
@@ -31,11 +49,16 @@ public class Checking extends Account {
 		}
 		return false;
 	}
-
+	
 	/**
-	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
-	 * continue to withdraw an overdrawn account until the balance is below -$100
-	 */
+	  Method: withdraw
+	  Inputs: amount: amount of money being taken from account's balance.
+	  Returns: Boolean. True if withdraw occurred and false if there was an error.
+
+	  Description: Withdraws given amount from the accounts' balance. After 10 withdrawals 
+	  a fee of $2 is charged per transaction You may continue to withdraw an overdrawn 
+	  account until the balance is below -$100
+	*/
 	public boolean withdraw(float amount) {
 		if (amount > 0.0f) {		
 			// KG: incorrect, last balance check should be >=
@@ -55,7 +78,17 @@ public class Checking extends Account {
 
 	public String getType() { return "Checking"; }
 	
+	/**
+	  Method: toString
+	  Inputs: none
+	  Returns: String form of Checking account.
+
+	  Description: Creates a string version of account with the form "Checking: name: balance"
+	*/
 	public String toString() {
 		return "Checking: " + getName() + ": " + getBalance();
 	}
+	
+	private static final long serialVersionUID = 11L;
+	private int numWithdraws = 0;
 }
