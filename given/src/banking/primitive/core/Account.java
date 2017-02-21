283ab2a79660dcde3b64f8d9749ad868fdc822fe
sullivan-wenger <swenger@asu.edu>
2017-02-20 21:04:20 -0700

    Update Account.java

--- given/src/banking/primitive/core/Account.java
+++ given/src/banking/primitive/core/Account.java
@@ -22,42 +22,62 @@ public abstract class Account implements java.io.Serializable {
     }
 
     /**
+     * Method: getName
+     * 
+     * @param N/A
      * @return name of the Account
+     * 
+     * Description: Get the name on the account.
      */
     public final String getName() {
         return name;
     }
 
     /**
+     * Method: getBalance
+     * 
+     * @param N/A
      * @return balance in the Account
+     * 
+     * Description: Get the current balance in the account
      */
     public final float getBalance() {
         return balance;
     }
 
     /**
-     * Adds money to an account. May not be done if the account is CLOSED
+     * Method: deposit
      * 
      * @param parameter
      *            amount is a deposit and must be > 0
      * @return true if the deposit was successful, false if not due to amount or
      *         invalid state
+     * 
+     * Description: Adds money to an account. May not be done if the account is CLOSED
      */
     public abstract boolean deposit(float amount);
 
     /**
-     * Takes money out of an account. If the balance falls below 0 then the
-     * account is moved to an OVERDRAWN state
+     * Method: withdraw
      * 
      * @param parameter
      *            amount is a withdrawal and must be > 0
      * @return true if the deposit was successful, false if not due to amount or
      *         invalid state
+     * 
+     * Description: 
+     * Takes money out of an account. If the balance falls below 0 then the
+     * account is moved to an OVERDRAWN state
      */
     public abstract boolean withdraw(float amount);
 
     /**
+     * Method: getType
+     * 
+     * @param N/A
      * @return either "Checking" or "Savings"
+     * 
+     * Description: Tell whether the account is checking or savings.
      */
     public abstract String getType();
 
@@ -70,9 +90,12 @@ public abstract class Account implements java.io.Serializable {
     }
     
     /**
-    * Describes the account in a human comprehensible way.
+    * Method: toString
     * 
+    * @param N/A
     * @return a string that matches the regex /Account .* has \$\d+\.\d+and is (OPEN|CLOSED|OVERDRAWN)/
+    * 
+    * Description: Describes the account in a human comprehensible way.
     */
     public String toString() {
         return "Account " + name + " has $" + balance + "and is " + getState()
